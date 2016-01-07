6e218200c51ae5ebe185ebebeea9787e4bd5f035
Giff Song <giff.song@gmail.com>
2016-01-07 18:46:06 +0900

    Fix failing test on linux

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,3 @@
-import * as es from 'event-stream';
 import * as Memorystream from 'memorystream';
 import * as chai from 'chai';
 import * as clc from 'cli-color';
@@ -8,19 +7,18 @@ import { Childminder } from '..';
 
 describe('Childminder', () => {
   describe('#create', () => {
-    it('should create child without prefix', done => {
+    it('should create child without prefix', async () => {
       const cm = new Childminder();
       const stream = new Memorystream(null, { readable: false });
       const child = cm.create('echo', ['hello'], {
         stdout: stream,
       });
-      child.terminal.on('end', () => {
-        expect(stream.toString()).to.equal('hello\n');
-        done();
-      });
+
+      await new Promise(resolve => child.terminal.on('exit', resolve));
+      expect(stream.toString()).to.equal('hello\n');
     });
 
-    it('should create child with prefix when option exists', done => {
+    it('should create child with prefix when option exists', async () => {
       const cm = new Childminder();
       const stream = new Memorystream(null, { readable: false });
       const child = cm.create('echo', ['hello'], {
@@ -28,14 +26,14 @@ describe('Childminder', () => {
         prefix: 'blabla',
         prefixColor: 120,
       });
-      child.terminal.on('end', () => {
-        expect(stream.toString())
-          .to.equal(`${clc.xterm(120)('[blabla]')} hello\n`);
-        done();
-      });
+
+      await new Promise(resolve => child.terminal.on('exit', resolve));
+      expect(stream.toString()).to.equal(
+        `${clc.xterm(120)('[blabla]')} hello\n`
+      );
     });
 
-    it('should be able to be called multiple times', done => {
+    it('should be able to be called multiple times', async () => {
       const cm = new Childminder();
       const stream1 = new Memorystream(null, { readable: false });
       const child1 = cm.create('echo', ['hello'], {
@@ -50,16 +48,16 @@ describe('Childminder', () => {
         prefixColor: 130,
       });
 
-      es.merge([
-        child1.terminal,
-        child2.terminal,
-      ]).on('end', () => {
-        expect(stream1.toString())
-          .to.equal(`${clc.xterm(120)('[blabla]')}    hello\n`);
-        expect(stream2.toString())
-          .to.equal(`${clc.xterm(130)('[blablabla]')} world\n`);
-        done();
-      });
+      await Promise.all([
+        new Promise(resolve => child1.terminal.on('exit', resolve)),
+        new Promise(resolve => child2.terminal.on('exit', resolve)),
+      ]);
+      expect(stream1.toString()).to.equal(
+        `${clc.xterm(120)('[blabla]')}    hello\n`
+      );
+      expect(stream2.toString()).to.equal(
+        `${clc.xterm(130)('[blablabla]')} world\n`
+      );
     });
   });
 });
@@ -73,14 +71,14 @@ describe('Child', () => {
       const child = cm.create('echo', ['hello'], {
         stdout: stream,
       });
-      await new Promise(resolve => child.terminal.on('end', resolve));
+      await new Promise(resolve => child.terminal.on('exit', resolve));
 
       // Restart two times.
       child.restart();
-      await new Promise(resolve => child.terminal.on('end', resolve));
+      await new Promise(resolve => child.terminal.on('exit', resolve));
 
       child.restart();
-      await new Promise(resolve => child.terminal.on('end', resolve));
+      await new Promise(resolve => child.terminal.on('exit', resolve));
 
       expect(stream.toString()).to.equal('hello\nhello\nhello\n');
     });
